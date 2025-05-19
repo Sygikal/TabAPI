f3af42044968ca6adf30017e39d46d7ebc288a42
Sygikal <xtin8873@gmail.com>
2025-05-18 23:01:41 -0700

    Add config

---
+++ src/main/java/dev/sygii/tabapi/TabAPIConfig.java
@@ -0,0 +1,11 @@
+package dev.sygii.tabapi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TabAPIConfig {
+
+    public boolean renderTabs = true;
+
+    public Map<String, Boolean> tabs = new HashMap<>();
+}
