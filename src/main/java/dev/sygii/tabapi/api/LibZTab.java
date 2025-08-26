64322c54c675cadefd87af91b7a23f33011feab8
Sygikal <xtin8873@gmail.com>
2025-08-26 11:44:06 -0700

    Pass real arguments and more compat

--- src/main/java/dev/sygii/tabapi/api/LibZTab.java
+++ src/main/java/dev/sygii/tabapi/api/LibZTab.java
@@ -13,10 +13,14 @@ import org.jetbrains.annotations.Nullable;
 @Environment(EnvType.CLIENT)
 public class LibZTab extends InventoryTab {
     private final LibZRunner runner;
+    private final LibZItemStackRunner stackRunner;
+    private final LibZShouldShowRunner showRunner;
 
-    public LibZTab(Text title, @Nullable Identifier texture, int preferedPos, LibZRunner runner, Class<?>... screenClasses) {
+    public LibZTab(Text title, @Nullable Identifier texture, int preferedPos, LibZRunner runner, LibZItemStackRunner stackRunner, LibZShouldShowRunner showRunner, Class<?>... screenClasses) {
         super(Identifier.of("libz", title.getString()), title, texture, preferedPos, screenClasses);
         this.runner = runner;
+        this.stackRunner = stackRunner;
+        this.showRunner = showRunner;
     }
 
     @Override
@@ -24,8 +28,27 @@ public class LibZTab extends InventoryTab {
         this.runner.asd(client);
     }
 
+    @Override
+    public boolean shouldShow(MinecraftClient client) {
+        return this.showRunner.run(client);
+    }
+
+    @Override
+    @Nullable
+    public ItemStack getItemStack(MinecraftClient client) {
+        return stackRunner.run(client);
+    }
+
     public interface LibZRunner {
         void asd(MinecraftClient client);
+    }
+
+    public interface LibZItemStackRunner {
+        ItemStack run(MinecraftClient client);
+    }
+
+    public interface LibZShouldShowRunner {
+        boolean run(MinecraftClient client);
     }
 
 }
--- src/main/java/dev/sygii/tabapi/mixin/compat/TabRegistryMixin.java
+++ src/main/java/dev/sygii/tabapi/mixin/compat/TabRegistryMixin.java
@@ -19,8 +19,15 @@ public class TabRegistryMixin {
 
     @Inject(method = "registerInventoryTab", at=@At("HEAD"), remap = false, cancellable = true)
     private static void inject(InventoryTab tab, CallbackInfo ci) {
-        System.out.println(Arrays.toString(((InventoryTabAccess) (tab)).getScreenClasses()));
-        TabAPI.registerInventoryTab(new LibZTab(tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, ((InventoryTabAccess)(tab)).getScreenClasses()));
+        TabAPI.LOGGER.info("Mapping {} tab from LibZ to TabAPI for {}", tab.getTitle(), ((InventoryTabAccess) (tab)).getScreenClasses());
+        TabAPI.registerInventoryTab(new LibZTab(tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()));
+        ci.cancel();
+    }
+
+    @Inject(method = "registerOtherTab", at=@At("HEAD"), remap = false, cancellable = true)
+    private static void injectOther(InventoryTab tab, Class<?> parentClass, CallbackInfo ci) {
+        TabAPI.LOGGER.info("Mapping {} other tab from LibZ to TabAPI for {}", tab.getTitle(), ((InventoryTabAccess) (tab)).getScreenClasses());
+        TabAPI.registerOtherTab(new LibZTab(tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()), parentClass);
         ci.cancel();
     }
 }
