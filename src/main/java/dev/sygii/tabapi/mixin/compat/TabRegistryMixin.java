fe15036c436d40b9308ea0138ec12030ca72ab44
Sygikal <xtin8873@gmail.com>
2025-08-29 11:39:06 -0700

    i really hate Text

--- src/main/java/dev/sygii/tabapi/mixin/compat/TabRegistryMixin.java
+++ src/main/java/dev/sygii/tabapi/mixin/compat/TabRegistryMixin.java
@@ -1,10 +1,14 @@
 package dev.sygii.tabapi.mixin.compat;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import dev.sygii.tabapi.TabAPI;
 import dev.sygii.tabapi.api.InventoryTabAccess;
 import dev.sygii.tabapi.api.LibZTab;
 import net.libz.api.InventoryTab;
 import net.libz.registry.TabRegistry;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,15 +23,19 @@ public class TabRegistryMixin {
 
     @Inject(method = "registerInventoryTab", at=@At("HEAD"), remap = false, cancellable = true)
     private static void inject(InventoryTab tab, CallbackInfo ci) {
-        TabAPI.LOGGER.info("Mapping {} tab from LibZ to TabAPI for {}", tab.getTitle(), ((InventoryTabAccess) (tab)).getScreenClasses());
-        TabAPI.registerInventoryTab(new LibZTab(tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()));
+        JsonElement asd = JsonParser.parseString(Text.Serializer.toJson(tab.getTitle()));
+        String unTranslated = asd.getAsJsonObject().get("translate").getAsString();
+        TabAPI.LOGGER.info("Mapping {} tab from LibZ to TabAPI for {}", unTranslated, ((InventoryTabAccess) (tab)).getScreenClasses());
+        TabAPI.registerInventoryTab(new LibZTab(unTranslated, tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()));
         ci.cancel();
     }
 
     @Inject(method = "registerOtherTab", at=@At("HEAD"), remap = false, cancellable = true)
     private static void injectOther(InventoryTab tab, Class<?> parentClass, CallbackInfo ci) {
-        TabAPI.LOGGER.info("Mapping {} other tab from LibZ to TabAPI for {}", tab.getTitle(), ((InventoryTabAccess) (tab)).getScreenClasses());
-        TabAPI.registerOtherTab(new LibZTab(tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()), parentClass);
+        JsonElement asd = JsonParser.parseString(Text.Serializer.toJson(tab.getTitle()));
+        String unTranslated = asd.getAsJsonObject().get("translate").getAsString();
+        TabAPI.LOGGER.info("Mapping {} other tab from LibZ to TabAPI for {}", unTranslated, ((InventoryTabAccess) (tab)).getScreenClasses());
+        TabAPI.registerOtherTab(new LibZTab(unTranslated, tab.getTitle(), tab.getTexture(), tab.getPreferedPos(), tab::onClick, tab::getItemStack, tab::shouldShow, ((InventoryTabAccess)(tab)).getScreenClasses()), parentClass);
         ci.cancel();
     }
 }
